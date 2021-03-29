93898a4f1ef48c38b57d61dc493d63c4f8f1c11c
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2021-03-29 17:11:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ import firebase from 'firebase'
 require("@firebase/firestore")
 
 
+
 var firebaseConfig = {
-    apiKey: "AIzaSyBSnonkksb-d2a0S6cFLZ6SgY8c363-9NY",
-    authDomain: "survey12345678901234567890.firebaseapp.com",
-    databaseURL: "https://survey12345678901234567890.firebaseio.com",
-    projectId: "survey12345678901234567890",
-    storageBucket: "survey12345678901234567890.appspot.com",
-    messagingSenderId: "288544834295",
-    appId: "1:288544834295:web:98c77533cc6c0e129f09f2"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBplLkm8W3T6JOJoSTd0gCl3A_GWB8ScVA",
+  authDomain: "stories-app123.firebaseapp.com",
+  projectId: "stories-app123",
+  storageBucket: "stories-app123.appspot.com",
+  messagingSenderId: "439320085738",
+  appId: "1:439320085738:web:58ebc58a44e43cd0ea0393"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
   export default firebase.firestore();
